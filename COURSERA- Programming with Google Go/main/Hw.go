1e7a45c86db7e20c1978b8415add9488271129ce
FedericoGaglio <56273271+FedericoGaglio@users.noreply.github.com>
2022-03-07 11:52:37 +0100

    Update Hw.go

--- COURSERA- Programming with Google Go/main/Hw.go
+++ COURSERA- Programming with Google Go/main/Hw.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	//"courserago/week1"
-	//"courserago/week2"
-	//"courserago/week3"
+	"courserago/week1"
+	"courserago/week2"
+	"courserago/week3"
 	"courserago/week4"
 )
 
 func main(){
 	//HW WEEK 1
-	//week1.PrintHello()
+	week1.PrintHello()
 
 	// HW WEEK 2
-	//week2.Trunc()
-	//week2.FindIAN()
+	week2.Trunc()
+	week2.FindIAN()
 
 	// HW WEEK 3
-	//week3.SliceInsert()
+	week3.SliceInsert()
 
 	// HW WEEK 4
-	//week4.MakeJSON()
+	week4.MakeJSON()
 	week4.InsertData()
 
 }
